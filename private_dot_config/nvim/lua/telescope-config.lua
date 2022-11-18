a683c0922a8d44067504dfbf4370130045333ca8
Helmer <11061182+helmerdavila@users.noreply.github.com>
2022-11-17 20:37:14 -0500

    Buffers picker by default using normal mode

--- private_dot_config/nvim/lua/telescope-config.lua
+++ private_dot_config/nvim/lua/telescope-config.lua
@@ -1,5 +1,10 @@
 require('telescope').setup {
   defaults = {
     file_ignore_patterns = {"node_modules", ".git"}
+  },
+  pickers = {
+    buffers = {
+      initial_mode = "normal"
+    }
   }
 }
