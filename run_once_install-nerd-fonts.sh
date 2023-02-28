251b8527a26300c1e0231f459e964100384b3281
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-02-27 23:33:46 -0500

    Fixing shell scripts and using bash as executable

--- run_once_install-nerd-fonts.sh
+++ run_once_install-nerd-fonts.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$(ls -A ~/.nerd-fonts)" ]; then
+echo "Hello from this script"
+  
+if [ -d "$HOME/.nerd-fonts" ]; then
   echo "Fonts already installed"
 else
-  (
     git clone --depth=1 https://github.com/ryanoasis/nerd-fonts ~/.nerd-fonts
     cd .nerd-fonts
     sudo ./install.sh
-  )
 fi
