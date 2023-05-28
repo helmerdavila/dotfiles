f0bc2072fe954dc8128003037949820b420a6423
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-05-28 17:43:12 -0400

    Adding shared sortcuts for Telescope

--- private_dot_config/nvim/lua/telescope-config.lua
+++ private_dot_config/nvim/lua/telescope-config.lua
@@ -20,6 +20,9 @@ telescope.setup {
     mappings = {
       n = {
         ["q"] = actions.close,
+        ["<leader>-"] = actions.select_horizontal,
+        ["<leader>0"] = actions.select_vertical,
+        ["<leader>9"] = actions.select_tab,
       },
     },
   },
