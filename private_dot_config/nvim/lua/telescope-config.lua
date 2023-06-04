6e0ff5093b72f7d6ebd62b57be39471e38cbd086
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-06-04 16:18:49 -0400

    Updating telescope theme

--- private_dot_config/nvim/lua/telescope-config.lua
+++ private_dot_config/nvim/lua/telescope-config.lua
@@ -16,7 +16,24 @@ keymap.set("n", "<leader>gs", builtin.git_status)
 
 telescope.setup {
   defaults = {
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    entry_prefix = "  ",
     file_ignore_patterns = {"node_modules", ".git"},
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    layout_strategy = "horizontal",
     mappings = {
       n = {
         ["q"] = actions.close,
@@ -25,6 +42,22 @@ telescope.setup {
         ["<leader>3"] = actions.select_horizontal,
       },
     },
+    path_display = { "truncate" },
+    prompt_prefix = "   ",
+    selection_caret = "  ",
+    set_env = { ["COLORTERM"] = "truecolor" },
+    sorting_strategy = "ascending",
+    vimgrep_arguments = {
+      "rg",
+      "-L",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    winblend = 0,
   },
   pickers = {
     buffers = {
@@ -32,7 +65,6 @@ telescope.setup {
     },
     find_files = {
       hidden = true,
-      theme = "dropdown",
     },
     git_status = {
       initial_mode = "normal",
