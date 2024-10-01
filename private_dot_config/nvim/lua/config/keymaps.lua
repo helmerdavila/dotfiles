fffd21b6ca7fb9fbac45c840660d07d7caef2f0f
Helmer <11061182+helmerdavila@users.noreply.github.com>
2024-09-30 19:33:04 -0600

    Removing bug related to keymaps

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local keymap = vim.keymap
-
 -- Remove \w commands
-keymap.del("n", "<leader>ww")
-keymap.del("n", "<leader>wd")
-keymap.del("n", "<leader>w-")
-keymap.del("n", "<leader>w|")
-
-keymap.set("n", "<leader>w", "<cmd>w<cr>")
-keymap.set("i", "jj", "<esc>")
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+vim.keymap.set("i", "jj", "<esc>")
